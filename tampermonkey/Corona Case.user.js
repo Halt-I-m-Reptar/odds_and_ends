e3f08498df6578aff05b9fad2887d672d56c9df0
Peter Creutzberger <pcreutzberger@gmail.com>
2020-03-29 14:41:51 -0400

    added image

--- tampermonkey/Corona Case.user.js
+++ tampermonkey/Corona Case.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Who doesn't like sarcasm?!
 // @author       Peter Creutzberger
-// @match        https://corona-check.herokuapp.com/
+// @match        https://corona-check.herokuapp.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -12,7 +12,7 @@
     'use strict';
 
     Array.from(document.getElementsByTagName("p")).forEach( (cur, index) => {
-        if ( index > 0 ) { cur.innerHTML = newCase(cur.innerHTML); }
+        if ( index > 0 ) { cur.innerHTML = '<img src="https://i.imgflip.com/1pq988.jpg" style="width:10%;height:10%;padding-right:10px;"/>' + newCase(cur.innerHTML); }
     });
 
     function newCase(text) {
